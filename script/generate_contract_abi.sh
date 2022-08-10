e07daaf6df18c8967ce485382ed674cced398feb
Amethyst <zhonghaoma@blackfish.cn>
2022-08-10 17:11:18 +0800

    -- delete unuseful files

--- script/generate_contract_abi.sh
+++
@@ -1,3 +0,0 @@
-# generate contract go file
-# should have contract abi file
-abigen --abi="../resources/contract/pancakeswap.abi.json" --type="Loot" --pkg=contract --out="../contract/Pancakeswap.go"
